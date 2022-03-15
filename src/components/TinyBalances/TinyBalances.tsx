62b61407861436af7d7a71aa7cee06d48e05351e
Brian Lee <brianlee1597@gmail.com>
2022-03-15 16:47:24 +0900

    changed ust parameters

--- src/components/TinyBalances/TinyBalances.tsx
+++ src/components/TinyBalances/TinyBalances.tsx
@@ -11,156 +11,191 @@ import SectionWrapper from 'components/SectionWrapper/SectionWrapper';
 import TxModal from 'components/Modal/TxModal';
 import { MsgExecuteContract, MsgSend } from '@terra-money/terra.js';
 
-export default function TinyBalances () {
+export default function TinyBalances() {
+  const ustSwapRateQuery = "https://fcd.terra.dev/v1/market/swaprate/uusd";
+  const lowerlimit = 0; //below this amount in UST will not be donatable
+  const general_ust_fee = toChainAmount(0.75); //for native balance error
 
-    const ustSwapRateQuery = "https://fcd.terra.dev/v1/market/swaprate/uusd"
-    const lowerlimit = 0 //below this amount in UST will not be donatable
-    const general_ust_fee = toChainAmount(0.04); //for native balance error
+  const LCD = useLCDClient();
+  const user_address = useAddress();
+  const { status } = useWallet();
 
-    const LCD = useLCDClient();
-    const user_address = useAddress();
-    const { status } = useWallet();
+  const [msgs, setMsgs] = useState<(MsgSend | MsgExecuteContract)[]>([]);
+  const [threshold, setThreshold] = useState<number>(2.5);
+  const [tinyBalances, setTinyBalances] = useState<any[]>([]);
+  const [tinyCW20s, setTinyCW20s] = useState<any[]>([]);
+  const [ustSwapRateMap, setUstSwapRateMap] = useState<any>(new Map());
+  const [mapPopulated, setMapPopulated] = useState<boolean>(false);
 
-    const [msgs, setMsgs] = useState<(MsgSend | MsgExecuteContract)[]>([]);
-    const [threshold, setThreshold] = useState<number>(2.5);
-    const [tinyBalances, setTinyBalances] = useState<any[]>([]);
-    const [tinyCW20s, setTinyCW20s] = useState<any[]>([]);
-    const [ustSwapRateMap, setUstSwapRateMap] = useState<any>(new Map())
-    const [mapPopulated, setMapPopulated] = useState<boolean>(false);
+  //remove individual tokens
+  const remove = (denom: string) => {
+    setTinyBalances((prev) => prev.filter((coin) => coin.denom !== denom));
+    setTinyCW20s((prev) => prev.filter((coin) => coin.denom !== denom));
+  };
 
-    //remove individual tokens
-    const remove = (denom: string) => {
-        setTinyBalances(prev => prev.filter(coin => coin.denom !== denom))
-        setTinyCW20s(prev => prev.filter(coin => coin.denom !== denom))
+  /* postable msgs populator */
+  const donate = async () => {
+    if (tinyBalances.length === 0 && tinyCW20s.length === 0) {
+      return;
     }
 
-    /* postable msgs populator */
-    const donate = async () => {
-        if ( tinyBalances.length === 0 && tinyCW20s.length === 0 ) {
-            return;
-        }
+    const balancesObj = tinyBalances.reduce((obj, el) => {
+      return Object.assign(obj, { [el.denom]: el.amount });
+    }, {});
 
-        const balancesObj = tinyBalances.reduce((obj, el) => {
-            return Object.assign(obj, { [el.denom]: el.amount })
-        }, {})
+    const donateNativeMsg = donateTinyAmount(
+      user_address,
+      ANGEL_PROTO_ADDRESS_MAIN,
+      balancesObj
+    );
+    const donateCW20Msgs = donateTinyCW20Amount(
+      user_address,
+      ANGEL_PROTO_ADDRESS_MAIN,
+      tinyCW20s
+    );
 
-        const donateNativeMsg = donateTinyAmount(user_address, ANGEL_PROTO_ADDRESS_MAIN, balancesObj);
-        const donateCW20Msgs = donateTinyCW20Amount(user_address, ANGEL_PROTO_ADDRESS_MAIN, tinyCW20s);
+    const msgs = [...donateNativeMsg, ...donateCW20Msgs];
+    setMsgs(msgs);
+  };
 
-        const msgs = [...donateNativeMsg, ...donateCW20Msgs];
-        setMsgs(msgs);
+  //function to update native tokens to donate on slider change
+  const tinyBalanceSetter = async () => {
+    if (!user_address || !mapPopulated) {
+      setTinyBalances([]);
+      return;
     }
 
-    //function to update native tokens to donate on slider change
-    const tinyBalanceSetter = async () => {
-        if ( !user_address || !mapPopulated ) {
-            setTinyBalances([]);
-            return;
-        }
+    const [coins] = await LCD.bank.balance(user_address);
 
-        const [coins] = await LCD.bank.balance(user_address);
+    const tinyBalances = coins
+      .toData()
+      .filter(
+        (coin) =>
+          ustValue(coin, ustSwapRateMap) > toChainAmount(lowerlimit) &&
+          ustValue(coin, ustSwapRateMap) < toChainAmount(threshold)
+      );
 
-        const tinyBalances = coins.toData()
-        .filter(coin => 
-        ustValue(coin, ustSwapRateMap) > toChainAmount(lowerlimit)
-        && ustValue(coin, ustSwapRateMap) < toChainAmount(threshold));
+    tinyBalances.forEach((coin, index) => {
+      if (coin.denom === "uusd") {
+        +coin.amount > general_ust_fee
+          ? (coin.amount = `${+coin.amount - general_ust_fee}`)
+          : tinyBalances.splice(index, 1);
+      }
+    });
+    setTinyBalances(tinyBalances);
+  };
 
-        tinyBalances.forEach((coin, index) => {
-            if (coin.denom === "uusd") {
-                + coin.amount > general_ust_fee ? 
-                coin.amount = `${+ coin.amount - general_ust_fee}` :
-                tinyBalances.splice(index, 1)
-            }
-        })
-        setTinyBalances(tinyBalances);
+  const tinyCW20Setter = async () => {
+    if (!user_address) {
+      setTinyCW20s([]);
+      return;
     }
 
-    const tinyCW20Setter = async () => {
-        if ( !user_address ) {
-            setTinyCW20s([]);
-            return;
-        }
+    let cw20s: any[] = [];
 
-        let cw20s: any[] = [];
-
-        for ( const [symbol, info] of Object.entries(cw20Tokens) ) {
-            const { balance }: any = await LCD.wasm.contractQuery(info.token_addr, {
-                balance: {
-                    address: user_address
-                },
-            })
-            cw20s.push({ denom: symbol, amount: String( balance ), address: info.token_addr });
-        }
-
-        cw20s = cw20s.filter(c => Number( c.amount ) > toChainAmount(lowerlimit)
-        && ustValue(c, ustSwapRateMap) < toChainAmount(threshold));
-        setTinyCW20s(cw20s);
+    for (const [symbol, info] of Object.entries(cw20Tokens)) {
+      const { balance }: any = await LCD.wasm.contractQuery(info.token_addr, {
+        balance: {
+          address: user_address,
+        },
+      });
+      cw20s.push({
+        denom: symbol,
+        amount: String(balance),
+        address: info.token_addr,
+      });
     }
 
-    const refetchUserState = () => {
-        tinyBalanceSetter();
-        tinyCW20Setter();
-    }
+    cw20s = cw20s.filter(
+      (c) =>
+        Number(c.amount) > toChainAmount(lowerlimit) &&
+        ustValue(c, ustSwapRateMap) < toChainAmount(threshold)
+    );
+    setTinyCW20s(cw20s);
+  };
 
-    useEffect(() => {
-        if (!user_address || !cw20Tokens) return;
+  const refetchUserState = () => {
+    tinyBalanceSetter();
+    tinyCW20Setter();
+  };
 
-        ;(async () => {
-            /* UST Swaprate to calculate all denominations into appropriate unified tiny amount limit */
-            const { data: swaprates } = await axios.get(ustSwapRateQuery);
-            const swapMap = swaprates.reduce((map: any, obj: any) => { map.set(obj.denom, + obj.swaprate); return map; }, new Map([["uusd", 1]]));
+  useEffect(() => {
+    if (!user_address || !cw20Tokens) return;
 
-            for ( const [symbol, info] of Object.entries(cw20Tokens) ) {
-                const swaprate = await getCW20Swaprate(info.liq_pool_addr);
+    (async () => {
+      /* UST Swaprate to calculate all denominations into appropriate unified tiny amount limit */
+      const { data: swaprates } = await axios.get(ustSwapRateQuery);
+      const swapMap = swaprates.reduce((map: any, obj: any) => {
+        map.set(obj.denom, +obj.swaprate);
+        return map;
+      }, new Map([["uusd", 1]]));
 
-                swapMap.set(symbol, swaprate);
-            }
+      for (const [symbol, info] of Object.entries(cw20Tokens)) {
+        const swaprate = await getCW20Swaprate(info.liq_pool_addr);
 
-            setUstSwapRateMap(swapMap);
-            setMapPopulated(true);
-        })();
-    }, [ user_address, cw20Tokens ]);
+        swapMap.set(symbol, swaprate);
+      }
 
-    //native token threshold slider callback
-    useEffect(() => {
-        if ( !user_address || ustSwapRateMap === undefined || !mapPopulated || !cw20Tokens ) return;
+      setUstSwapRateMap(swapMap);
+      setMapPopulated(true);
+    })();
+  }, [user_address, cw20Tokens]);
 
-        tinyBalanceSetter();
-        tinyCW20Setter();
-    }, [ user_address, ustSwapRateMap, mapPopulated, cw20Tokens ]);
-
-    //just for threshold with timeout
-    useEffect(() => {
-        if ( !user_address || ustSwapRateMap === undefined || !mapPopulated ) return;
-
-        const getTinyBalances = setTimeout(() => {
-            tinyBalanceSetter();
-            tinyCW20Setter();
-        }, 200);
-        return () => clearTimeout(getTinyBalances);
-    }, [ threshold ]);
-
-    useEffect( refetchUserState , [ status ])
-
-    return (
-        <>
-        <TxModal msgs={msgs}/>
-        <SectionWrapper type="balance">
-            <TokenContainer 
-            donate={donate}
-            button_desc="Donate Angel Dust"
-            desc="No Angel Dust Available"
-            array={[...tinyBalances, ...tinyCW20s]} swapMap={ustSwapRateMap} remove={ remove }/>
-            <DescriptionSubmit 
-            title={<h1>Donate Your<br/>Angel Dust</h1>}
-            subtitle="Clean out the small balances in your wallet by giving it to a good cause."
-            threshold_desc="Small Balance Threshold:"
-            threshold={threshold}
-            setThreshold={setThreshold}
-            donate={donate}
-            button_desc="Donate Angel Dust"
-            />
-        </SectionWrapper>
-        </>
+  //native token threshold slider callback
+  useEffect(() => {
+    if (
+      !user_address ||
+      ustSwapRateMap === undefined ||
+      !mapPopulated ||
+      !cw20Tokens
     )
+      return;
+
+    tinyBalanceSetter();
+    tinyCW20Setter();
+  }, [user_address, ustSwapRateMap, mapPopulated, cw20Tokens]);
+
+  //just for threshold with timeout
+  useEffect(() => {
+    if (!user_address || ustSwapRateMap === undefined || !mapPopulated) return;
+
+    const getTinyBalances = setTimeout(() => {
+      tinyBalanceSetter();
+      tinyCW20Setter();
+    }, 200);
+    return () => clearTimeout(getTinyBalances);
+  }, [threshold]);
+
+  useEffect(refetchUserState, [status]);
+
+  return (
+    <>
+      <TxModal msgs={msgs} />
+      <SectionWrapper type="balance">
+        <TokenContainer
+          donate={donate}
+          button_desc="Donate Angel Dust"
+          desc="No Angel Dust Available"
+          array={[...tinyBalances, ...tinyCW20s]}
+          swapMap={ustSwapRateMap}
+          remove={remove}
+        />
+        <DescriptionSubmit
+          title={
+            <h1>
+              Donate Your
+              <br />
+              Angel Dust
+            </h1>
+          }
+          subtitle="Clean out the small balances in your wallet by giving it to a good cause."
+          threshold_desc="Small Balance Threshold:"
+          threshold={threshold}
+          setThreshold={setThreshold}
+          donate={donate}
+          button_desc="Donate Angel Dust"
+        />
+      </SectionWrapper>
+    </>
+  );
 }
